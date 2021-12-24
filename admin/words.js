0791268b556f3b5654ae304dac640535494f184c
BrummF <35379524+BrummF@users.noreply.github.com>
2021-12-24 16:50:24 +0100

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'cab adapter settings': {
+        'en': 'Adapter settings for cab',
+        'de': 'Adaptereinstellungen für cab',
+        'ru': 'Настройки адаптера для cab',
+        'pt': 'Configurações do adaptador para cab',
+        'nl': 'Adapterinstellingen voor cab',
+        'fr': "Paramètres d'adaptateur pour cab",
+        'it': "Impostazioni dell'adattatore per cab",
+        'es': 'Ajustes del adaptador para cab',
+        'pl': 'Ustawienia adaptera dla cab',
+        'zh-cn': 'cab的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
